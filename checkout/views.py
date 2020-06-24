a7ad812a475598d5086c792b4c6a9b7b2e0ed426
Pauloa90 <paulo.a90@hotmail.com>
2020-06-24 09:15:41 +0000

    adding feedback code to checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -8,9 +8,9 @@ from django.utils import timezone
 from products.models import Product
 import stripe
 
-# Create your views here.
 
 stripe.api_key = settings.STRIPE_SECRET
+
 
 @login_required()
 def checkout(request):
@@ -34,29 +34,28 @@ def checkout(request):
                     quantity=quantity
                 )
                 order_line_item.save()
+            
             try:
                 customer = stripe.Charge.create(
-                    amount = int(total * 100),
-                    currency = "EUR",
-                    description = request.user.email,
-                    card = payment_form.cleaned_data['stripe_id'],
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id']
                 )
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
-
+            
             if customer.paid:
-                messages.error(request, "You have succefully paid!")
+                messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
                 return redirect(reverse('products'))
             else:
                 messages.error(request, "Unable to take payment")
-
         else:
             print(payment_form.errors)
             messages.error(request, "We were unable to take a payment with that card!")
-            
     else:
         payment_form = MakePaymentForm()
         order_form = OrderForm()
-
-    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
+    
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
