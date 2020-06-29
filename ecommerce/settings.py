aa7fe8dabf23448793925b98109418d76bfe3273
Pauloa90 <paulo.a90@hotmail.com>
2020-06-29 19:10:22 +0000

    fixing travis testing

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from os import path
+#from os import path
 #if path.exists("env.py"):
 #    import env 
 import dj_database_url
