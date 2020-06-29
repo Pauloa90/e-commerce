21cd7a4db7fd404466e3e9356230de03dc27c5a4
Pauloa90 <paulo.a90@hotmail.com>
2020-06-29 22:42:22 +0000

    fixing bugs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-#from os import path
-#if path.exists("env.py"):
-#    import env 
+from os import path
+if path.exists("env.py"):
+    import env 
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
